c86c22f271c84e0a92a2e3ca0d331c42c245859c
Ahmad Daneshvar <topcoder.romixery@gmail.com>
2024-05-17 14:39:42 -0400

    Update Cocoapods and SPM

--- SwiftStomp/Classes/Enumurations/StompUpstreamMessage.swift
+++ SwiftStomp/Classes/Enumurations/StompUpstreamMessage.swift
@@ -5,6 +5,8 @@
 //  Created by Ahmad Daneshvar on 5/16/24.
 //
 
+import Foundation
+
 public enum StompUpstreamMessage {
     case text(message : String, messageId : String, destination : String, headers : [String : String])
     case data(data: Data,  messageId : String, destination : String, headers : [String : String])
